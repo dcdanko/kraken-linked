6eb9c2482b36a9ff04a010b987c7b292927ee067
David Danko <dcdanko@gmail.com>
2018-12-17 15:29:48 -0500

    bugfix - fixing compilation errors

--- src/seqreader.cpp
+++ src/seqreader.cpp
@@ -31,7 +31,7 @@ namespace kraken {
   vector<DNASequence> BCReader::next_bc() {
     vector<DNASequence> bc;
     DNASequence next_seq;
-    while (cur_seq.id == NULL){
+    while (cur_seq == NULL){
       cur_seq = bc_fastq_reader.next_sequence();
     }
     bc.push_back(cur_seq);
